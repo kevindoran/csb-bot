5d9f91aafad002b7b952f526929cd6d141591d9c
Kevin <kevinxdoran@gmail.com>
2016-08-22 15:44:13 +1200

    Chance simulation's checkpoint layout.

--- src/simulationMain.cpp
+++ src/simulationMain.cpp
@@ -5,7 +5,7 @@
 #include "AnnealingBot.h"
 #include "PodracerBot.h"
 
-Race r1(3, {Vector(10000, 3400), Vector(11200, 5400), Vector(7200, 6700), Vector(5400, 2700)});
+Race r1(3, {Vector(1000, 340), Vector(11200, 2400), Vector(14200, 7700), Vector(5400, 8700)});
 
 int main(int argc, char* argv[]) {
     ostream* out;
@@ -16,7 +16,8 @@ int main(int argc, char* argv[]) {
     } else {
         out = &cout;
     }
-    DuelBot* b1 = new AnnealingBot();
+    DuelBot* b1 = new AnnealingBot(r1, 140);
+//    DuelBot* b1 = new TraditionalBot();
     DuelBot* b2 = new TraditionalBot();
     Simulation sim(r1);
     GameHistory gh = sim.simulate(b1, b2);
