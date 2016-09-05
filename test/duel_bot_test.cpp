60973df2f9fd9dae1deed19ebd9c4285b7941127
kevindoran <kevinxdoran@gmail.com>
2016-09-05 15:03:09 +1200

    Test to insure correct eval fctn behaviour near checkpoint edge.

--- test/duel_bot_test.cpp
+++ test/duel_bot_test.cpp
@@ -38,7 +38,7 @@ protected:
 };
 
 TEST_F(DuelBotTest, annealing_bot_test) {
-    AnnealingBot bot(r);
+    AnnealingBot<6> bot(r);
     for(int i = 0; i < 50; i++) {
         cout << "#" << i << endl;
         PairOutput control = bot.move(gs);
@@ -47,20 +47,20 @@ TEST_F(DuelBotTest, annealing_bot_test) {
 
 TEST(DuelBotTestNoFixture, annealing_bot_score) {
     Race race(3, {Vector(0,0), Vector(15000,0), Vector(17000, 10000)});
-    AnnealingBot bot(race);
+    AnnealingBot<6> bot(race);
     PodState p1pod1Pre(Vector(5000, 0), Vector(0,0), 0, 1);
     PodState p1pod2Pre(Vector(5000, 0), Vector(0,0), 0, 1);
     PodState p2pod1Pre(Vector(5000, 0), Vector(0,0), 0, 1);
     PodState p2pod2Pre(Vector(5000, 0), Vector(0,0), 0, 1);
 
     // Option 1 (Should be lower score)
-    PodState p1pod1(Vector(15000, 0), Vector(0, 0), 0, 1);
+    PodState p1pod1(Vector(15000-POD_RADIUS, 0), Vector(0, 0), 0, 1);
     PodState p1pod2(Vector(0, 0), Vector(0, 0), 0, 1);
     PodState p2pod1(Vector(7000, 0), Vector(0, 0), 0, 1);
     PodState p2pod2(Vector(8000, 0), Vector(0, 0), 0, 1);
 
     // Option 2
-    PodState p1pod12(Vector(15001, 0), Vector(0, 0), 0, 2);
+    PodState p1pod12(Vector(15001-POD_RADIUS, 0), Vector(0, 0), 0, 2);
 
     const PodState* podsPre[] = {&p1pod1Pre, &p1pod2Pre};
     const PodState* enemyPodsPre[] = {&p2pod1Pre, &p2pod2Pre};
@@ -71,6 +71,26 @@ TEST(DuelBotTestNoFixture, annealing_bot_score) {
     float outsideCP = bot.score(pods1, podsPre, enemyPods1, enemyPodsPre);
     float insideCP = bot.score(pods2, podsPre, enemyPods1, enemyPodsPre);
     ASSERT_GT(outsideCP, insideCP);
+}
+
+
+TEST(DuelBotTestNoFixture, annealing_bot_progess) {
+    Race race(3, {Vector(0,0), Vector(15000, 0)});
+    AnnealingBot<6> bot(race);
+    PodState prev;
+    PodState cur;
+    float prevScore = -1000;
+    for(int x = 0; x <= 15000; x++) {
+        prev = PodState(Vector(x-400-500, 0), Vector(0, 0), 0, 1);
+        cur = PodState(Vector(x-400, 0), Vector(0, 0), 0, 1);
+        ASSERT_GT(bot.progress(&cur, &prev), prevScore);
+    }
+    prev = PodState(Vector(15000-400-100, 0), Vector(0, 0), 0, 1);
+    cur = PodState(Vector(15001-400, 0), Vector(0, 0), 0, 0);
+    ASSERT_GT(bot.progress(&cur, &prev), prevScore);
+    prev = PodState(Vector(15000-400-50, 0), Vector(0, 0), 0, 1);
+    cur = PodState(Vector(15100-400, 0), Vector(0, 0), 0, 0);
+    ASSERT_GT(bot.progress(&cur, &prev), prevScore);
 }
 
 int main(int argc, char **argv) {
